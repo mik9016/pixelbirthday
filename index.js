635dcc334796702d785621ecf20d24f9150978e6
Mikolaj Gruszecki <mikolaj.gruszecki@sdui.de>
2023-06-28 15:50:57 +0200

    input handling added

--- index.js
+++ index.js
@@ -6,6 +6,47 @@ const mainImage = document.getElementById('mainImage')
 const home = document.getElementById('home')
 const end = document.getElementById('end')
 
+const input0Wrapper = document.getElementById('input0Wrapper')
+const input0 = document.getElementById('letter0')
+const introBtn = document.getElementById('introBtn')
+
+const input1Wrapper = document.getElementById('input1Wrapper')
+const input1 = document.getElementById('letter1')
+const mapBtn = document.getElementById('mapBtn')
+
+const input2Wrapper = document.getElementById('input2Wrapper')
+const input2 = document.getElementById('letter2')
+const playgroundBtn = document.getElementById('playgroundBtn')
+
+const input3Wrapper = document.getElementById('input3Wrapper')
+const input3 = document.getElementById('letter3')
+const iceBtn = document.getElementById('iceBtn')
+
+function showError(element){
+    const node = document.createElement("p");
+    const textnode = document.createTextNode("Spróbuj jeszcze raz! :)");
+    node.classList.add('error')
+    node.appendChild(textnode);
+    element.appendChild(node);
+}
+
+function enableBtn(btn){
+    btn.disabled = false
+}
+
+
+const answers = {
+    3: 'A',
+    0: 'E',
+    1: 'R',
+    2: 'T',
+}
+
+
+function validate(input,answer) {
+    return input.value.toString() === answer
+}
+
 function showEnd(){
     end.classList.remove('unvisible')
     mainImage.src="./assets/hpb.jpg"
@@ -21,6 +62,10 @@ function showHome(){
     mainImage.src="./assets/sleep.jpg"
 }
 function hideIce(){
+    if(!validate(input3,answers[2])){
+        showError(input3Wrapper)
+        return
+    }
     ice.classList.add('unvisible')
     showHome()
 }
@@ -30,6 +75,10 @@ function showIce(){
 }
 
 function hidePlayground(){
+    if(!validate(input2,answers[1])){
+        showError(input2Wrapper)
+        return
+    }
     playground.classList.add('unvisible')
     showIce()
 }
@@ -40,6 +89,10 @@ function showPlayground(){
 }
 
 function hideMaps(){
+    if(!validate(input1,answers[0])){
+        showError(input1Wrapper)
+        return
+    }
     map.classList.add('unvisible')
     showPlayground()
 }
@@ -50,6 +103,10 @@ function showText(){
 }
 
 function hideIntroText(){
+    if(!validate(input0,answers[3])){
+        showError(input0Wrapper)
+        return
+    }
     introText.classList.add('unvisible')
     showText()
 }
